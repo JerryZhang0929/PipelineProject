4f3b451999f17e16d7672364fa60f32fb6a9a82e
ZHANG Jerry (CI/DAV3.9) <Jerry.ZHANG2@cn.bosch.com>
2019-08-21 14:22:07 +0800

    First commit

---
+++ unit-tests/src/test/java/cn/bosch/UnitTest.java
@@ -0,0 +1,12 @@
+package cn.bosch;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UnitTest {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
